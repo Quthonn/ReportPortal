b43715cb6a20dfcfee33efaee4216c0496cad30c
quthonn <gevorgyan.genri@gmail.com>
2025-05-10 00:13:29 +0700

    Simplifying the code

--- src/test/java/apiTests/CreateNewDashboard.java
+++ src/test/java/apiTests/CreateNewDashboard.java
@@ -9,6 +9,8 @@ import static org.hamcrest.Matchers.hasItem;
 import io.restassured.RestAssured;
 import java.util.List;
 import java.util.Locale;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
 
 import com.github.javafaker.Faker;
 
@@ -40,35 +42,15 @@ public class CreateNewDashboard {
                 .log().all()
                 .statusCode(201);
 
-        List<String> allDashboardNames = new ArrayList<>();
-        int page = 1;
-        int size = 20;
-        int totalPages;
-
-        do {
-            String response = given()
-                    .header("Authorization", "Bearer " + TOKEN)
-                    .contentType("application/json")
-                    .queryParam("page", page)
-                    .queryParam("size", size)
-                    .when()
-                    .get("/api/v1/default_personal/dashboard?page.size=10000")
-                    .then()
-                    .log().all()
-                    .statusCode(200)
-                    .extract().asString();
-
-            JsonPath jsonPath = new JsonPath(response);
-            List<String> dashboardNames = jsonPath.getList("content.name");
-            if (dashboardNames != null) {
-                allDashboardNames.addAll(dashboardNames);
-            }
-            totalPages = jsonPath.getInt("page.totalPages");
-            page++;
-        } while (page <= totalPages);
-
-        System.out.println("Все имена Dashboard'ов: " + allDashboardNames);
-        assertThat("Dashboard должен присутствовать в списке", allDashboardNames, hasItem(dashboardName));
+        given()
+                .header("Authorization", "Bearer " + TOKEN)
+                .contentType("application/json")
+                .when()
+                .get("/api/v1/default_personal/dashboard?page.size=10000")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("content.name", hasItem(dashboardName));
     }
-
 }
+
